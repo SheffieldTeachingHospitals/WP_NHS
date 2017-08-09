2464f7393e25cb1afdab94e95667597d073c99d6
Andrew Duckworth <grillopress@gmail.com>
2017-08-09 10:13:14 +0000

    add slider marketing part

--- template-parts/marketing-parts/content-category-feed-cards.php
+++ template-parts/marketing-parts/content-category-feed-cards.php
@@ -21,12 +21,13 @@ if( $is_published ): ?>
       <?php endif;?>
       
       
-      
       <?php if( get_sub_field( 'cfc_category' ) ): ?>
       
         <?php $cfc_feed_cat = get_sub_field( 'cfc_category' );
+              $cfc_feed_no = get_sub_field( 'cfc_number' );
+              $cfc_count = 0;
               $cfc_cat_feed = new WP_Query( array( 'cat' => $cfc_feed_cat,
-                                                        'posts_per_page' => 3,
+                                                        'posts_per_page' => $cfc_feed_no,
                                                         'orderby'    => 'date',
                                                         'order'      => 'DESC',
                                                         ) );
@@ -34,10 +35,10 @@ if( $is_published ): ?>
         <?php while ( $cfc_cat_feed->have_posts() ) : $cfc_cat_feed->the_post(); ?>
       
       
-      <div class="col-md-4">
+      <div class="col-md-4" id="cat-card-<?php echo $cfc_count ;?>">
           <?php if ( has_post_thumbnail() ) :?>
                 <a href="<?php the_permalink() ;?>">
-                  <?php the_post_thumbnail('medium', array('class' => 'img-responsive img-full')); ?>
+                  <?php the_post_thumbnail('marketing-5-3-cropped', array('class' => 'img-responsive img-full')); ?>
                 </a>
               <?php else :?>
                 <a href="<?php the_permalink() ;?>">
@@ -49,8 +50,8 @@ if( $is_published ): ?>
               <?php the_excerpt(); ?>
           </div>
       </div>
-        
-      
+      <?php $cfc_count++ ;?>
+      <?php if ($cfc_count == 3) echo "<div class='col-md-12'></div>" ;?>
         <?php endwhile; ?>
         <?php wp_reset_postdata(); ?>
         <?php endif;?>
